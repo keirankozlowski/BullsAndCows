3f2074a17031ed542d6755ea1839b59902ec06ea
Keiran <keirankozlowski@gmail.com>
2018-12-22 18:40:46 -0500

    Set up gitignores.

--- BullsAndCows/main.cpp
+++ BullsAndCows/main.cpp
@@ -1,11 +1,29 @@
 #include <iostream>
+#include <string>
 using namespace std;
 
 int main() {
+	// constant expressions
 	constexpr int WORD_LENGTH = 5;
 
+	// game intro
 	cout << "Welcome to Bulls and Cows, a fun word game!" << endl;
-	cout << "Can you guess the " << WORD_LENGTH << " letter isogram I'm thinking of?" << endl;
+	cout << "Can you guess the " << WORD_LENGTH << " letter isogram I'm thinking of?" << endl << endl;
+
+	// get a guess from the player
+	string PlayerGuess = "";
+	cout << "Enter your guess: ";
+	getline(cin, PlayerGuess);
+
+	// repeat the guess back to the player
+	cout << "Your guess was: " << PlayerGuess << endl << endl;
+
+	//// get a guess from the player
+	cout << "Enter your guess: ";
+	getline(cin, PlayerGuess);
+
+	// repeat the guess back to the player
+	cout << "Your guess was: " << PlayerGuess << endl << endl;
 
 	return 0;
 }
